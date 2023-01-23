cb51d31d148001b60301b9af1ecb499b0d896d73
Pablo Aragón <pablo.aragon22@gmail.com>
2023-01-23 12:04:38 +0100

    fix mqtt loop

--- src/mqtt.py
+++ src/mqtt.py
@@ -6,15 +6,21 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     log.info("Connected with result code "+str(rc))
 
+def on_disconnect(client, userdata, rc):
+   global flag_connected
+   log.info("Mqtt disconnected")
+
 
 def init():
     global client
     log.info("Connecting MQTT")
     client = mqtt.Client()
     client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
     client.connect(os.environ["MQTT_SERVER"],
                    int(os.environ["MQTT_PORT"]), int(os.environ["MQTT_KEEPALIVE"]))
+    client.loop_start()
 
 
 def publish(topic, payload):
-    client.publish(topic, payload)
+    client.publish(topic, payload, qos=2)
